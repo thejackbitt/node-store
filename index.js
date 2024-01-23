b0e5ceaa090a4a14037a21fe6f0fc13ccabbfbb5
Jonathon Blom <blomjonathon@gmail.com>
2024-01-23 14:10:32 -0600

    added functionalilty for then block, made changes to index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const inquirer = require("inquirer")
-const Store = require("./lib/Store")
-const Product = require("./lib/Product")
-const Customer = require("./lib/Customer")
+const inquirer = require("inquirer");
+const Store = require("./lib/Store");
+const Product = require("./lib/Product");
+const Customer = require("./lib/Customer");
 
-// let's set up some objects for use 
-const widget1 = new Product("Widget 2000", 20, 50)
-const widget2 = new Product("Widget 5000", 50, 20)
-const widget3 = new Product("Widget 8000", 100, 10)
+// let's set up some objects for use
+const widget1 = new Product("Widget 2000", 20, 50);
+const widget2 = new Product("Widget 5000", 50, 20);
+const widget3 = new Product("Widget 8000", 100, 10);
 
-const currentStore = new Store("Gary Mart", 9, 16, [widget1, widget2, widget3] )
+const currentStore = new Store("Gary Mart", 9, 16, [widget1, widget2, widget3]);
 
 // Use inquirer to welcome the use to the store, and show a list of products for then to choose from
-function start(){
-  console.log(currentStore.displayWelcome())
-  
+function start() {
+  console.log(currentStore.displayWelcome());
+
   inquirer
     .prompt([
       {
         type: "list",
         message: "What product would you like to buy?",
         choices: [
-          {value: "widget1"},
-          {value: "widget2"},
-          {value: "widget3"},
+          { name: widget1.name,
+            value: widget1
+          },
+          { name: widget2.name,
+            value: widget2
+          },
+          { name: widget3.name,
+            value: widget3
+          },
         ],
         name: "selectedProduct",
       },
@@ -32,17 +38,23 @@ function start(){
         name: "customerName",
       },
     ])
-
+    .then((response) => {
+      const newCustomer = new Customer(response.customerName);
+      currentStore.addShopper(newCustomer)
+      newCustomer.buyProduct(currentStore, response.selectedProduct);
+      console.log(currentStore.shoppersClub)
+    });
 }
 
 // after the customer chooses a product, get their name
 // create a customer object; add customer to the shoppers club
-// use the buy method so the user buy the product 
-// send them back to the start menu
-function buyProduct(){
 
+
+// use the buy method so the user buy the product
+// send them back to the start menu
+function buyProduct() {
+  
 }
 
 
-
-start()
+start();
