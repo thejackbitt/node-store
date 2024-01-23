c58ccdfd6105971b059960dd2b888b44a89d91a1
RB <jsriley24505@gmail.com>
2024-01-23 12:20:01 -0600

    setup the test for product.test.js file

--- test/Product.test.js
+++ test/Product.test.js
@@ -3,14 +3,20 @@ const Product = require("../lib/Product")
 describe("Product", () => {
   describe("Instantiation", () => {
     it("should instantiate correctly", () => {
-     
+     const milk = new Product("Milk", 2.99, 12)
+     expect(milk).toBeInstanceOf(Product)
+     expect(milk.name).toBe("Milk")
+     expect(milk.price).toBe(2.99)
+     expect(milk.qty).toBe(12)
       // TODO: make sure an object is instantiated 
     })
   })
 
   describe("reduceQty method", () => {
-    it("should reduce the qty correctly", () => {
-      // TODO (see "it" statement )
+    it("should reduce the qty of the product(milk) correctly", () => {
+      const milk = new Product("Milk", 2.99, 12);
+      milk.reduceQty();
+      expect(milk.qty).toBe(11);
     })
   })
 })
