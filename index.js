e9c4e5f2e4d0749357f943d9458fde427d40e458
j-preim <j.preimesberger@gmail.com>
2024-01-23 13:08:42 -0600

    added prompts

--- index.js
+++ index.js
@@ -8,11 +8,30 @@ const widget1 = new Product("Widget 2000", 20, 50)
 const widget2 = new Product("Widget 5000", 50, 20)
 const widget3 = new Product("Widget 8000", 100, 10)
 
-const currentStore = new Store("your-store-name", 9, 16, [widget1, widget2, widget3] )
+const currentStore = new Store("Gary Mart", 9, 16, [widget1, widget2, widget3] )
 
 // Use inquirer to welcome the use to the store, and show a list of products for then to choose from
 function start(){
-  currentStore.displayWelcome();
+  console.log(currentStore.displayWelcome())
+  
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What product would you like to buy?",
+        choices: [
+          {value: "widget1"},
+          {value: "widget2"},
+          {value: "widget3"},
+        ],
+        name: "selectedProduct",
+      },
+      {
+        type: "input",
+        message: "Great choice. What is your name?",
+        name: "customerName",
+      },
+    ])
 
 }
 
