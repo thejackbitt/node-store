1aba8d5a2ba48ae8894664f93a129d1e786cf6cf
Dan Noonan <dnoonan23@yahoo.com>
2024-01-23 14:08:18 -0600

    merge

--- test/Store.test.js
+++ test/Store.test.js
@@ -5,10 +5,6 @@ describe("Store", () => {
   describe("Instantiation", () => {
     it("should instantiate correctly", () => {
       // TODO: make sure an object is instantiated
-      const testStore = new Store('abcd', 5, 12);
-        expect(testStore.name).toBe('abcd');
-        expect(testStore.opens).toBe(5);
-        expect(testStore.closes).toBe(12);
     })
   })
 
@@ -23,6 +19,8 @@ describe("Store", () => {
   describe("Add product to inventory", () => {
     it("should add a product to inventory correctly", () => {
       // TODO (see "it" statement )
+      const prod = new Product ('Stuff', 4.99, 5);
+      
     })
   })
 })
