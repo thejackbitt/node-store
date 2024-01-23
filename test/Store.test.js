f6975af7608aec88e933fe6d0c1116f3747a868a
Dan Noonan <dnoonan23@yahoo.com>
2024-01-23 12:56:19 -0600

    fixed some store stuff

--- test/Store.test.js
+++ test/Store.test.js
@@ -5,6 +5,10 @@ describe("Store", () => {
   describe("Instantiation", () => {
     it("should instantiate correctly", () => {
       // TODO: make sure an object is instantiated
+      const testStore = new Store('abcd', 5, 12);
+        expect(testStore.name).toBe('abcd');
+        expect(testStore.opens).toBe(5);
+        expect(testStore.closes).toBe(12);
     })
   })
 
